cffb04d2d08bb93821df0d25f5cfdabc564b8825
seub <sebastien.tersin@free.fr>
2011-08-18 03:29:33 +0200

    Added new classes BlockType and Block

---
+++ WorldCraft/WorldCraft/Block.cs
@@ -0,0 +1,36 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+#endregion
+
+namespace WorldCraft
+{
+    #region BlockType
+
+    public enum BlockType : byte
+    {
+        None,
+        Rock,
+        Grass
+    }
+
+    #endregion
+
+    #region Block
+
+    public struct Block
+    {
+        public BlockType Type;
+        
+        public Block(BlockType blockType)
+        {
+            Type = blockType;
+        }
+    }
+
+    #endregion
+
+}
